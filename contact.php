3549ecfd166766883e7525246c1ec8bef0304e19
zanehuggins <54915657+zanehuggins@users.noreply.github.com>
2020-05-25 10:55:44 -0400

    .

---
+++ contact.php
@@ -0,0 +1,35 @@
+<?php
+$field_name = $_POST['First-Name'];
+$field_email = $_POST['E-Mail'];
+$field_phone = $_POST['Phone'];
+$field_message = $_POST['Message'];
+$field_subject = $_POST['Subject'];
+
+/* change the email address on line 10 to match yours */
+/* you can change the text on line 11, 'Message from ' to whatever you wish. This will be displayed in the subject line of your email*/
+
+$mail_to = 'championtrophiesandawards@gmail.com';
+$subject = 'Message from '.$field_name;
+
+$body_message = 'From: '.$field_name."\r\n";
+$body_message .= 'E-mail: '.$field_email."\r\n";
+$body_message .= 'Phone: '.$field_phone."\r\n";
+$body_message .= 'Message: '.$field_message;
+
+$mail_status = mail($mail_to, $subject, $body_message);
+
+if ($mail_status) { ?>
+ <script language="javascript" type="text/javascript">
+  alert('Thank you for the message. We will contact you shortly.');
+  window.location = 'contact.html';
+ </script>
+<?php
+}
+else { ?>
+ <script language="javascript" type="text/javascript">
+  alert('Message sending failed. Please, send an email to yourname@email.com');
+  window.location = 'contact.html';
+ </script>
+<?php
+}
+?>
